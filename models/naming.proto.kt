866c47fffcc803e4ee92f5847528f51515274b11
Kirill Obukhov <kirill.obukhov@gmail.com>
2026-02-06 16:51:48 +0300

    .

--- models/naming.proto.kt
+++ models/naming.proto.kt
@@ -7,29 +7,27 @@ import kotlinx.serialization.protobuf.*
   ENUM_VALUE_NAME(0),
 }
 @Serializable data class MessageName(
-  @ProtoNumber(1) val scalarFieldName: Int = 0,
-  @ProtoOneOf val oneOfFieldName: IOneOfFieldName = IOneOfFieldName.IsoStringField(),
+  @ProtoNumber(1) @ProtoType(ProtoIntegerType.DEFAULT) val scalarFieldName: Int = 0,
+  @ProtoOneOf val oneOfFieldName: IOneOfFieldName? = null,
 ) {
   @Serializable sealed interface IOneOfFieldName {
     @Serializable data class IsoStringField (
       @ProtoNumber(2) val value: String = "",
     ) : IOneOfFieldName
     @Serializable data class LinuxTimestampField (
-      @ProtoNumber(3) val value: Long = 0L,
+      @ProtoNumber(3) @ProtoType(ProtoIntegerType.DEFAULT) val value: Long = 0L,
     ) : IOneOfFieldName
   }
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
     other as MessageName
-    return true &&
-      scalarFieldName.equals(other.scalarFieldName) &&
-      oneOfFieldName.equals(other.oneOfFieldName)
+    return scalarFieldName.equals(other.scalarFieldName) &&
+      oneOfFieldName?.equals(other.oneOfFieldName) ?: (other.oneOfFieldName === null)
   }
   override fun hashCode(): Int {
-    var result = 0
-    result = 31 * result + scalarFieldName.hashCode()
-    result = 31 * result + oneOfFieldName.hashCode()
+    var result = scalarFieldName.hashCode()
+    result = 31 * result + (oneOfFieldName?.hashCode() ?: 0)
     return result
   }
 }
--- models/scalar.proto.kt
+++ models/scalar.proto.kt
@@ -9,51 +9,41 @@ import kotlinx.serialization.protobuf.*
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
     other as Decimal
-    return true &&
-      floatValue.equals(other.floatValue) &&
+    return floatValue.equals(other.floatValue) &&
       doubleValue.equals(other.doubleValue)
   }
   override fun hashCode(): Int {
-    var result = 0
-    result = 31 * result + floatValue.hashCode()
+    var result = floatValue.hashCode()
     result = 31 * result + doubleValue.hashCode()
     return result
   }
 }
 @Serializable data class Integer(
-  @ProtoNumber(1) val variableSignedInt32: Int = 0,
-  @ProtoNumber(2) val variableSignedInt64: Long = 0L,
-  @ProtoNumber(3) val variableUnsignedInt32: UInt = 0U,
-  @ProtoNumber(4) val variableUnsignedInt64: ULong = 0UL,
-  @ProtoNumber(5) @ProtoType(ProtoIntegerType.FIXED) val fixedUnsignedInt32: UInt = 0U,
-  @ProtoNumber(6) @ProtoType(ProtoIntegerType.FIXED) val fixedUnsignedInt64: ULong = 0UL,
-  @ProtoNumber(7) @ProtoType(ProtoIntegerType.SIGNED) val zigzagSignedInt32: Int = 0,
-  @ProtoNumber(8) @ProtoType(ProtoIntegerType.SIGNED) val zigzagSignedInt64: Long = 0L,
+  @ProtoNumber(1) @ProtoType(ProtoIntegerType.DEFAULT) val signedVariableInt32: Int = 0,
+  @ProtoNumber(2) @ProtoType(ProtoIntegerType.DEFAULT) val signedVariableInt64: Long = 0L,
+  @ProtoNumber(5) @ProtoType(ProtoIntegerType.SIGNED) val signedZigzagInt32: Int = 0,
+  @ProtoNumber(6) @ProtoType(ProtoIntegerType.SIGNED) val signedZigzagInt64: Long = 0L,
+  @ProtoNumber(7) @ProtoType(ProtoIntegerType.FIXED) val unsignedFixedInt32: UInt = 0U,
+  @ProtoNumber(8) @ProtoType(ProtoIntegerType.FIXED) val unsignedFixedInt64: ULong = 0UL,
 ) {
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
     other as Integer
-    return true &&
-      variableSignedInt32.equals(other.variableSignedInt32) &&
-      variableSignedInt64.equals(other.variableSignedInt64) &&
-      variableUnsignedInt32.equals(other.variableUnsignedInt32) &&
-      variableUnsignedInt64.equals(other.variableUnsignedInt64) &&
-      fixedUnsignedInt32.equals(other.fixedUnsignedInt32) &&
-      fixedUnsignedInt64.equals(other.fixedUnsignedInt64) &&
-      zigzagSignedInt32.equals(other.zigzagSignedInt32) &&
-      zigzagSignedInt64.equals(other.zigzagSignedInt64)
+    return signedVariableInt32.equals(other.signedVariableInt32) &&
+      signedVariableInt64.equals(other.signedVariableInt64) &&
+      signedZigzagInt32.equals(other.signedZigzagInt32) &&
+      signedZigzagInt64.equals(other.signedZigzagInt64) &&
+      unsignedFixedInt32.equals(other.unsignedFixedInt32) &&
+      unsignedFixedInt64.equals(other.unsignedFixedInt64)
   }
   override fun hashCode(): Int {
-    var result = 0
-    result = 31 * result + variableSignedInt32.hashCode()
-    result = 31 * result + variableSignedInt64.hashCode()
-    result = 31 * result + variableUnsignedInt32.hashCode()
-    result = 31 * result + variableUnsignedInt64.hashCode()
-    result = 31 * result + fixedUnsignedInt32.hashCode()
-    result = 31 * result + fixedUnsignedInt64.hashCode()
-    result = 31 * result + zigzagSignedInt32.hashCode()
-    result = 31 * result + zigzagSignedInt64.hashCode()
+    var result = signedVariableInt32.hashCode()
+    result = 31 * result + signedVariableInt64.hashCode()
+    result = 31 * result + signedZigzagInt32.hashCode()
+    result = 31 * result + signedZigzagInt64.hashCode()
+    result = 31 * result + unsignedFixedInt32.hashCode()
+    result = 31 * result + unsignedFixedInt64.hashCode()
     return result
   }
 }
@@ -64,13 +54,10 @@ import kotlinx.serialization.protobuf.*
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
     other as Logical
-    return true &&
-      boolean.equals(other.boolean)
+    return boolean.equals(other.boolean)
   }
   override fun hashCode(): Int {
-    var result = 0
-    result = 31 * result + boolean.hashCode()
-    return result
+    return boolean.hashCode()
   }
 }
 @Serializable data class Sequence(
@@ -81,13 +68,11 @@ import kotlinx.serialization.protobuf.*
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
     other as Sequence
-    return true &&
-      bytesSequence.contentEquals(other.bytesSequence) &&
+    return bytesSequence.contentEquals(other.bytesSequence) &&
       charsSequence.equals(other.charsSequence)
   }
   override fun hashCode(): Int {
-    var result = 0
-    result = 31 * result + bytesSequence.contentHashCode()
+    var result = bytesSequence.contentHashCode()
     result = 31 * result + charsSequence.hashCode()
     return result
   }
